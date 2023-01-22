b374528edc523d1ea4295a21e13e12a34bf55918
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-01-22 20:00:35 +0100

    update 2 task vin player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,11 @@
 import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', ({seconds}) => {
-    console.log(seconds);
-});
+player.on('timeupdate', throttle(e => {
+    localStorage.setItem("videoplayer-current-time",e.seconds)
+},1000));
 
-console.log(player)
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
